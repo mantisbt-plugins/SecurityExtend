ff356c88cc5ae94e9f1f92d2679ffa3a007b3de0
spmeesseman <spmeesseman@gmail.com>
2019-08-03 11:06:42 -0400

    fix(domains): blocked email domains are not saving

--- pages/securityextend_edit.php
+++ pages/securityextend_edit.php
@@ -8,23 +8,33 @@ auth_reauthenticate();
 access_ensure_global_level(config_get('manage_plugin_threshold'));
 access_ensure_global_level(config_get('edit_threshold_level'));
 
-$f_block_bug = gpc_get_string('block_bug');
-$f_block_bug_disable_user = gpc_get_string('block_bug_disable_user');
-$f_block_bug_delete_user = gpc_get_string('block_bug_delete_user');
+$f_tab= gpc_get_string('tab');
 
-save_config_value('block_bug', $f_block_bug);
-save_config_value('block_bug_disable_user', $f_block_bug_disable_user);
-save_config_value('block_bug_delete_user', $f_block_bug_delete_user);
+if ($f_tab == plugin_lang_get('management_block_bug_title'))
+{
+    $f_block_bug = gpc_get_string('block_bug');
+    $f_block_bug_disable_user = gpc_get_string('block_bug_disable_user');
+    $f_block_bug_delete_user = gpc_get_string('block_bug_delete_user');
+
+    save_config_value('block_bug', $f_block_bug);
+    save_config_value('block_bug_disable_user', $f_block_bug_disable_user);
+    save_config_value('block_bug_delete_user', $f_block_bug_delete_user);
+}
+else if ($f_tab == plugin_lang_get('management_block_domain_title'))
+{
+    $f_block_domain = gpc_get_string('block_domain');
+    save_config_value('block_domain', $f_block_domain);
+}
 
 #
 # Done, cleanup, show success, and redirect
 #
 
-form_security_purge( 'plugin_SecurityExtend_securityextend_edit' );
+form_security_purge('plugin_SecurityExtend_securityextend_edit');
 
-$t_redirect_url = plugin_page('securityextend', TRUE);
+$t_redirect_url = plugin_page('securityextend', TRUE) . '&tab=' . $f_tab;
 
-layout_page_header( null, $t_redirect_url );
+layout_page_header(null, $t_redirect_url);
 layout_page_begin();
-html_operation_successful( $t_redirect_url );
+html_operation_successful($t_redirect_url);
 layout_page_end();
